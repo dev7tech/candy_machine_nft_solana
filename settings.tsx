cca89b66673f8f157715b417f8ae9dd7b0e4011c
Jacob <jacob.devtech102@gmail.com>
2024-05-22 12:48:42 -0600

    [Updated] mini button

---
+++ settings.tsx
@@ -0,0 +1,17 @@
+// Purpose: Contains texts that will be used in the UI when rendering the buttons
+// label has to be the exact same as in your candy machine config
+export const mintText = [
+  {
+    label: 'default',
+    mintText: 'Mint SOLDOG NFT',
+    buttonLabel: 'Mint now!',
+    header: 'Default',
+  },
+];
+
+//header image in the ui. replace with your own
+export const image =
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZsa0xJkv9MgCJpMoRMtRkMshIQ1xQc0VVzA&usqp=CAU';
+
+//website title
+export const headerText = 'SolDOG NFT';
